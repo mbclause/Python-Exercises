8bd9e85782d4f5d85829e871e3ca687a6fb501e6
Michael Clausen <90015476+mbclause@users.noreply.github.com>
2023-10-19 18:30:28 -0700

    created exercise 1

---
+++ string exercises/1.py
@@ -0,0 +1,9 @@
+# Write a program to accept two numbers from the user and calculate multiplication
+
+num1 = input("Enter a number: ")
+
+num2 = input("Enter a second number: ")
+
+result = num1 * num2
+
+print(result)
