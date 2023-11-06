40c0474b252fcd5fae555d3fc6983ec3e4519593
Michael Clausen <90015476+mbclause@users.noreply.github.com>
2023-11-05 16:57:07 -0800

    created exercise 1

---
+++ control flow exercises/1.py
@@ -0,0 +1,12 @@
+# Print First 10 natural numbers using while loop
+
+def print_first_ten():
+    count = 1
+
+    while(count < 11):
+        print(count)
+
+        count += 1
+
+
+print_first_ten()
