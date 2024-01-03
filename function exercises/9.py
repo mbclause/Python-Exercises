8c961c6cef94146a678d26d54db1ca43f6b0650a
Michael Clausen <90015476+mbclause@users.noreply.github.com>
2024-01-02 16:11:39 -0800

    created exercise 9

---
+++ function exercises/9.py
@@ -0,0 +1,18 @@
+'''
+Find the largest item from a given list
+
+x = [4, 6, 8, 24, 12, 2]
+
+Expected Output:
+
+24
+'''
+
+def find_max(myList):
+    return max(myList)
+
+x = [4, 6, 8, 24, 12, 2]
+
+res = find_max(x)
+
+print(res)
